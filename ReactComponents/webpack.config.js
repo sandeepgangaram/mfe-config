1c6bd1f05ca6ad60a1a8c71a2baf895dabfe4dad
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-10 18:58:14 +0530

    render header dynamically

--- ReactComponents/webpack.config.js
+++ ReactComponents/webpack.config.js
@@ -24,6 +24,7 @@ module.exports = {
       exposes: {
         "./MovieCard": "./src/components/MovieCard/MovieCard.jsx",
         "./BuyButton": "./src/components/Button/BuyButton/BuyButton.jsx",
+        "./Typography": "./src/components/Typography/Typography.jsx",
       },
     }),
     new MiniCssExtractPlugin(),
