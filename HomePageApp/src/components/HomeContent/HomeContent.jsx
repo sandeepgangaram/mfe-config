bb2517235d2348b2bf86ec301d8423edc1de7415
Sandeep Gangaram <sandeepgangaram@alumni.iitm.ac.in>
2023-04-10 18:18:42 +0530

    add fetch movie data in homepage

--- HomePageApp/src/components/HomeContent/HomeContent.jsx
+++ HomePageApp/src/components/HomeContent/HomeContent.jsx
@@ -2,13 +2,15 @@ import React, { useEffect, useState } from "react";
 import QuickBooking from "../QuickBooking/QuickBooking.jsx";
 import "./HomeContent.scss";
 
-const dummyItem = [{name:"Dummy Movie"}]
+const dummyItem = [{ name: "Dummy Movie" }];
 
 const HomeContent = (props) => {
   const [movies, setMovies] = useState(dummyItem);
 
-  useEffect(() => {
-    // Add the logic to load the movies from server and set to the state
+  useEffect(async () => {
+    const res = await fetch("http://localhost:5555/movies");
+    const data = await res.json();
+    console.log(data);
   }, []);
 
   const movieClicked = (item) => {
@@ -33,9 +35,7 @@ const HomeContent = (props) => {
   return (
     <div className="home-content-container">
       <QuickBooking></QuickBooking>
-      <div className="movies-container">
-        {renderMovieList()}
-      </div>
+      <div className="movies-container">{renderMovieList()}</div>
     </div>
   );
 };
